0d8a0f5bbd3994f0644deb039640577983eb07d7
atirson <fabiano.oliveira2213@gmail.com>
2022-04-07 20:51:12 -0300

    feat: Created method to filter user and movie by params

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,11 +1,31 @@
-const { UserList } = require("../FakeData")
+const { UserList, MovieList } = require("../FakeData")
 
 
 const resolvers = {
   Query: {
-    users() {
+    users: () => {
       return UserList
     },
+    user: (_, args) => {
+      const id = Number(args.id)
+
+      return UserList.find(user => user.id === id)
+    },
+
+    movies: () => {
+      return MovieList
+    },
+
+    movie: (_, args) => {
+      const name = args.name;
+
+      return MovieList.find((movie) => movie.name === name)
+    }
+  },
+  User: {
+    favoriteMovies: () => {
+      return MovieList.filter(movie => movie.yearOfPublication < 2012)
+    }
   }
 }
 
